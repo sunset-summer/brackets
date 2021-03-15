f45ecb6fab719f29cbab2608891757e8f48eab69
sunset-summer <alex1123321@gmail.com>
2021-03-15 04:14:40 +0200

    feat: add solution to task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let strArr = str.split("");
+    let strArrOriginal = strArr.slice();
+    while (strArr.length >= 1) {
+        for (let i = 0; i < strArr.length; i++) {
+            for (let k = 0; k < bracketsConfig.length; k++) {
+                if (strArr[i] == bracketsConfig[k][0]) {
+                    if (strArr[i + 1] == bracketsConfig[k][1]) {
+                        strArr.splice(i, 2);
+                        break;
+                    } else if (strArr[i + 1] == undefined) {
+                        return false;
+                    }
+                }
+            }
+        }
+        if (strArr.length == strArrOriginal.length) {
+            return false;
+        }
+        strArrOriginal = strArr.slice();
+    }
+    if (strArr.length == 0) {
+        return true;
+    } else {
+        return false;
+    }
+};
